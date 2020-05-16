216abc54d9ecda5268f931654a8fd052a1a61712
Jeothen <Jeothen@gmail.com>
2020-05-16 22:51:57 +0900

    BOJ 12837

--- BOJ/Segment Tree/12837/Project1/소스.cpp
+++ BOJ/Segment Tree/12837/Project1/소스.cpp
@@ -1,20 +1,36 @@
+#include <iostream>
+using namespace std;
+#pragma warning(disable:4996)
 
+const int N = 1000000;
+typedef long long ll;
+ll seg[4 * N];
+int start = 1;
+
+void update(int idx, int val) {
+	seg[idx] += val;	
+	while (idx > 1) {
+		idx /= 2;
+		seg[idx] = seg[idx * 2] + seg[idx * 2 + 1];
+	}
+}
+
+ll get_sum(int left, int right, int node, int nodeleft, int noderight) {
+	if (noderight < left || nodeleft > right) return 0;
+	if (left <= nodeleft && noderight <= right) return seg[node];
+	int mid = (nodeleft + noderight) >> 1;
+	return get_sum(left, right, 2 * node, nodeleft, mid) + get_sum(left, right, 2 * node + 1, mid + 1, noderight);
+}
 
 int main() {
-	0; // int
-	0.0; // double
-	nullptr; // std::nullptr_t
-
-	int* p1 = nullptr;
-	char* p2 = nullptr;
-	void(*f)() = nullptr;
-
-//	int n1 = nullptr;   error
-	int n2 = 0;
-
-	// bool type은 직접 초기화는 허용하지만 복사 초기화는 허용되지 않는다
-//	bool b1 = nullptr; error
-	bool b2(nullptr);
-	bool b3{ nullptr };
-//	bool b4 = { nullptr; } error
+	freopen("input.txt", "r", stdin);
+	int n, q;
+	scanf("%d %d", &n, &q);
+	while (n > start) start *= 2;
+	int a, p, x;
+	for (int i = 0; i < q; i++) {
+		scanf("%d %d %d", &a, &p, &x);
+		if (a == 1) update(start+p-1, x);
+		else printf("%lld\n", get_sum(start+p-1, start+x-1,1, start, 2 * start - 1));
+	}
 }
