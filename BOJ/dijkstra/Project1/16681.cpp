7b628be1a3c54fda015653f0b454531390aeca8f
Taehyun Jung <jeothen@gmail.com>
2020-08-17 11:36:36 +0900

    BOJ 16681

--- BOJ/dijkstra/Project1/16681.cpp
+++ BOJ/dijkstra/Project1/16681.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <queue>
 #include "string.h"
 using namespace std;
 #define MAX_NODE 100'001
-#define INF 1'000'000'000
+#define INF 1987654321987654321
 
 typedef long long ll;
 
+bool visited[MAX_NODE];
 int N,M,D,E; int height[MAX_NODE];
 vector <pair<int,int>> vc[MAX_NODE];
 
@@ -18,17 +19,20 @@ ll dist[MAX_NODE][2];
 void dijkstra(int start){
     int col = 0;
     if (start == N) col = 1;
-    bool     visited[MAX_NODE] = {0,};
-    priority_queue<pair<ll,int> > pq; //거리, point
- //   memset(visited,false,sizeof(visited));
-
+    priority_queue<pair<ll,int>> pq; //거리, point
+    memset(visited,false,sizeof(visited));
+    
     pq.push({0,start});
     while(!pq.empty()){
-        ll acc = -pq.top().first; int now = pq.top().second;
-        visited[now] = true;
+        
+        ll acc = -pq.top().first;
+        int now = pq.top().second;
         pq.pop();
-        for (int i = 0; i < vc[now].size();i++){
-            int dis = vc[now][i].first, next = vc[now][i].second;
+        if (visited[now]) continue;
+        visited[now] = true;
+
+        for (auto i : vc[now]){
+            ll dis = i.first, next = i.second;
             if (!visited[next] && height[next] > height[now]) {
                 if (acc + dis < dist[next][col]){
                     dist[next][col] = acc + dis;
@@ -50,16 +54,17 @@ int main(){
         vc[b].push_back({n,a});
     }
 
-    ll ans = -INF, tmp = -INF;
-    for (int i=2; i<N;i++){
-        dist[i][0] = INF; dist[i][1] = INF;
-    }
+    ll ans = -INF;
+    
+    fill(&dist[0][0], &dist[MAX_NODE][2], INF);
+    dist[1][0] = 0; dist[N][1] = 0;
+    
     dijkstra(1); dijkstra(N);
 
     for (int i=2; i<N; i++){
         if (dist[i][0] == INF || dist[i][1] == INF) continue;
-        tmp = height[i] * E - D * (dist[i][0] + dist[i][1]);
-        ans =  max(tmp,ans);
+        ll tmp = height[i] * E - D * (dist[i][0] + dist[i][1]);
+        ans =  (ans < tmp) ? tmp : ans;
     }
     if (ans != -INF) printf("%lld\n", ans);
     else printf("Impossible\n");
--- BOJ/dijkstra/Project1/2211.cpp
+++ BOJ/dijkstra/Project1/2211.cpp
@@ -1,40 +1,42 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <queue>
 #include <string.h>
 using namespace std;
 #define node 1001
-#define INF 100'000'000
+#define INF 100000000
 int n, dist[node];
+
 vector <pair<int,int>> vc[node];
+int link[node];
 
 void dijkstra(){
-    memset(dist,100,sizeof(dist));
-    for (int i=2;i<=n;i++) cout << dist[i] << endl;
+    fill(dist, dist+node, INF);
+    fill(link, link + node, -1);
     dist[1] = 0;
     priority_queue<pair<int,int>>pq;  // distance, index
-    bool visit[node]; memset(visit,false,sizeof(visit));
-    cout << endl;
-    pq.push({0,1});
+
+    pq.push({0,1}); 
     while(!pq.empty()){
         int val = pq.top().first, idx = pq.top().second;
-        visit[idx] = true;
         pq.pop();
         for (auto i=0; i<vc[idx].size(); i++){
-            int d = vc[idx][i].first, nidx = vc[idx][i].second;
-            cout << d << endl;
-            if (!visit[nidx]){
-                if (d + val < dist[nidx]){
-                    dist[nidx] = d + val;
-                    pq.push({dist[nidx], nidx});
-                }
-            } 
+            int d = vc[idx][i].first, next = vc[idx][i].second;
+            if (d + val < dist[next]) {
+                dist[next] = d + val; // minimum distance
+                pq.push({ dist[next], next });
+                link[next] = idx;
+            }
         }
+
     }
-    for (int i=2; i<=n;i++){
-        if (dist[i] != INF){
-            printf("%d %d\n", i, dist[i]);
-        }
+    int cnt = 0;
+    for (int i=1; i<=n;i++){
+        if (link[i] != -1) cnt++;
+    }
+    printf("%d\n",cnt);
+    for (int i=1; i<=n; i++){
+        if (link[i] !=-1) printf("%d %d\n",i, link[i]);
     }
 }
 
@@ -44,6 +46,7 @@ int main(){
     int a,b,c;
     for (int i=0; i<m; i++){
         scanf("%d %d %d",&a,&b,&c);
+        
         vc[a].push_back({c,b});
         vc[b].push_back({c,a});
     }
