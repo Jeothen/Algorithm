55b69febbc6d11247d909f6ba9c2ec477a4694e8
Taehyun Jung <jeothen@gmail.com>
2020-08-20 23:26:16 +0900

    not solving problem 1162

--- BOJ/dijkstra/Project1/1162.cpp
+++ BOJ/dijkstra/Project1/1162.cpp
@@ -1,20 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
-#define ROAD 10000
+#define city 10001
+#define packing 21
+#define INF 100'000'000'000
 typedef long long ll;
+ll dist[city][packing];
+vector <pair<ll,int>> vc[city];
 
-bool chk[ROAD][ROAD];
-vector <pair<ll,int>> vc[ROAD];
+int N,M,K,a,b,c;
+
+void init(){
+    scanf("%d %d %d", &N, &M, &K);
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        vc[a].push_back({c, b});
+        vc[b].push_back({c, a});
+    }
+    for (int i=1; i<=N;i++){
+        for (int j=0; j<=K;j++) {
+            if (i==1) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
+    }
+}
+
+void dijkstra(){
+    int s = 1, e = N;
+    priority_queue<pair<ll,pair<int,int>>>pq; // value, city, k 
+    pq.push({0,{1,0}});
+    while(!pq.empty()){
+        ll acc = pq.top().first;
+        int city_num = pq.top().second.first, k_val = pq.top().second.second;
+        pq.pop();
+        if (acc != dist[city_num][k_val])  continue;
+        
+        for (auto info : vc[city_num]) {
+            if (info.first + acc < dist[info.second][k_val]) {
+                dist[info.second][k_val] = info.first + acc;
+                pq.push({dist[info.second][k_val],{info.second,k_val}});
+            }
+            if (k_val < K) {
+                if (acc < dist[info.second][k_val+1])
+                {
+                    dist[info.second][k_val+1] = acc;
+                    pq.push({dist[info.second][k_val+1], {info.second, k_val+1}});
+                }
+            }
+        }
+    }
+
+}
 
 int main(){
     freopen("1162.txt","r",stdin);
-    int N,M,K,a,b,c;
-    scanf("%d %d %d",&N,&M,&K);
-    for (int i=0; i< M; i++){
-        scanf("%d %d %d",&a,&b,&c);
-        vc[a].push_back({c,b});
-        vc[b].push_back({c,a});
+    init();
+    dijkstra();
+    ll maximum = INF;
+    for (int i=0; i<=K; i++) {
+        maximum = (dist[N][i] < maximum) ? dist[N][i] : maximum;
     }
-
+    printf("%lld\n",maximum);
 }
