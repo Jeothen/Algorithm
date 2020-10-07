a15340bca7a1767e53a1f773c7db604ccdc3f376
jeothen <jeothen@gmail.com>
2020-10-08 00:23:02 +0900

    belmanforld basic

--- BOJ/Bellman Fold/tempCodeRunnerFile.cpp
+++ BOJ/Bellman Fold/tempCodeRunnerFile.cpp
@@ -14,14 +14,14 @@ int main(){
         scanf("%d %d %d",&a,&b,&c);
         vc[a].push_back({b,c});
     }
-    fill(dist, dist+n+1, INF);
+    fill(dist, dist+n, INF);
     dist[1] = 0;
     bool cycle = false;
     for (int i=0; i<n; i++){ // loop
         for (int j=1; j<=n; j++) { // 시작점
             for (auto &p: vc[j]){
                 int next = p.first, val = p.second;
-                if (dist[j] != INF && dist[next] > dist[j] + val) {
+                if (dist[next] != INF && dist[next] > dist[j] + val) {
                     dist[next] = dist[j] + val;
                     if (i == n-1) cycle = true; // last loop에서 update
                 } 
@@ -30,8 +30,6 @@ int main(){
     }
     if (cycle) printf("-1\n");
     else {
-        for (int i=2;i<=n;i++) {
-            printf("%lld\n",dist[i]!=INF?dist[i]:-1);
-        }
+        for (int i=2;i<=n;i++) printf("%lld\n",dist[i]!=INF?dist[i]:-1);
     }
 }
