5a3ab759fd5b596459109c07255abad87c1ff2e9
jeothen <jeothen@gmail.com>
2020-12-05 17:14:57 +0900

    bellman ford

--- Basic/Bellman_ford/Bellman_Ford.cpp
+++ Basic/Bellman_ford/Bellman_Ford.cpp
@@ -1,49 +1,38 @@
-// BOJ 11657
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#pragma warning(disable:4996)
-using namespace std;
-#define node 501
-const int INF = 1234567890;
-typedef pair<int, int> P;
-vector <P> vc[node];
-int dist[501],n,m;
-bool cycle;
-
-void bellman_ford() {
-	for (int i = 1; i<= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (auto &p : vc[j]) {
-				int next = p.first, d = p.second;
-				if (dist[j] != INF && dist[next] > dist[j] + d) {
-					dist[next] = dist[j] + d;
-					if (i == n) cycle = true;
-				}
-			}
-		}
-	}
+struct Edge {
+    int src;
+    int dest;
+    int weight;
 }
 
-int main() {
-	freopen("input.txt", "r", stdin);
-	cin >> n >> m;
-	int a, b, c;
-	for (int i = 0; i < m; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-		vc[a].push_back({ b,c });
-	}
-	fill(dist, dist + n+1, INF);
-	dist[1] = 0;
-	cycle = false;
-	bellman_ford();
-	if (cycle) {
-		cout << -1;
-	}
-	else {
-		for (int i = 2; i <= n; i++) {
-			printf("%d\n", dist[i] != INF ? dist[i] : -1);
-		}
-	}
+vector <Edge> edges;
+// or vector <Edge> edges[nodecount]
+
+void bellman_ford(int src) {
+    int* dist = malloc(sizeof(int) * nodecount);
+    // initial
+    for (int i=0; i < nodecount; i++){
+        if (i == src) dist[i] = 0;
+        else dist[i] = INF;
+    }
+    
+    // opeation
+    for (int i=0; i < nodecount; i++) {
+        for (int j=0; j < edgecount; j++) {
+            if (dist[edges[j] != INF && dist[edges[j].dest] > dist[edges[j].src] + edges[j].weight) {
+                dist[edges[j].dest] = dist[edges[j].src] + edges[j].weight;
+            }
+        }
+    }
+    // check
+    for (int i=0; i< edgecount; i++){
+        if (dist[edges[i].dest] > dist[edges[i].src] + edges[i].weight)
+        {
+            assert(cycle);
+            break;
+        }
+    }
+    for (int i=0; i < nodecount ; i++){
+        cout << "SSP %d = " << dist[i] << endl;
+    }
 }
+
---
+++ Basic/Bellman_ford/Readme.md
@@ -0,0 +1,13 @@
+# Bellman Ford
+
+
+
+* It is one of the algorithms to find the single source shortest distance.
+* It takes longer than Dijkstra in terms of time complexity, but it can also be calculated with negative weights.
+
+
+
+* In the process of finding a path, dist can be updated at max n-1 times (the number of nodes).
+  * Since the order of update is changed according to the order of node visits, the update can be completed by going through the n-1 loop except for the first node.
+* However, if an update occurs on the n times as well, there is a negative cycle.
+
