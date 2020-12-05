c5ae3ed7a54af476aa9406a0d643d968066057e5
jeothen <jeothen@gmail.com>
2020-12-06 00:49:16 +0900

    TSP

---
+++ Basic/TSP/Readme.md
@@ -0,0 +1,7 @@
+# TSP
+
+* NP that finds the minimum cost for all paths from the starting point
+* The TSP problem basically has the time complexity (n!) of all permutations, which are visitable paths.
+
+* TSP can be calculated using DP and bit masking.
+
---
+++ Basic/TSP/TSP.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#pragma warning(disable:4996)
+#define INF 1e9;
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+
+char lists[17];
+int n, cnt, arr[17][17], dp[17][1 << 16];
+
+// Traveling Salesman Problem
+int TSP(int cur, int visit)  // cur : current / visit = sum of visit city  - 5 : 0 1 0 1
+{
+	if (visit == (1 << n) - 1) // all city visit   ( if n == 4  ->  visit == 16000 - 1 = 1111 : 1 2 3 4)
+	{
+	/*	if (arr[cur][0] != 0) // possible to return start point
+			return arr[cur][0];
+		return INF;				// Imposibble to return start point
+	*/
+		return arr[cur][0] ? arr[cur][0] : INF;
+	}
+	int& ret = dp[cur][visit];
+	if (ret != -1) return ret; // already visit  cur -> visit 
+
+	ret = INF;
+	for (int i=0; i<n; i++){
+		if (visit & (1 << i)) continue; // already vit city i
+		if (arr[cur][i] == 0) continue; // not destinated
+		ret = MIN(ret, arr[cur][i] + TSP(i, visit | (1 << i))); // minimum distance
+	}
+	return ret;
+}
+
+void Solve(){
+	memset(dp, -1, sizeof(dp)); // 4 (int) * 2^16 (1<<16) * 17
+	printf("%d\n", TSP(0, 1));	// if INF, Not TSP;
+}
