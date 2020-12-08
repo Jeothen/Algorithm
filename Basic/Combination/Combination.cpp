a53df3f69b86b0bc46c6cfaa20af59190dd93f2a
jeothen <jeothen@gmail.com>
2020-12-08 22:54:11 +0900

    combination

--- Basic/Combination/Combination.cpp
+++ Basic/Combination/Combination.cpp
@@ -1,33 +1,54 @@
-#include <stdio.h>
-
+#include <iostream>
+using namespace std;
 
 int data[10];
-int t[10];
+int t1[10];
+int t2[10];
 int n;
-int count;
+int count1, count2;
 
-void combination(int n, int k, int q) {
+void combination(int n, int k, int q) { // q is just display count number
 	if (k == 0) {
-		count++;
-		for (int i = 0; i < q; i++)
-			printf("%d ", t[i]);
+		count1++;
+		for (int i = 0; i < q; i++) 
+			printf("%d ", t1[i]);
 		printf("\n");
 	}
 	else if (n < k) return;
 	else {
-		t[k - 1] = data[n - 1];
+		t1[k - 1] = data[n - 1]; 
 		combination(n - 1, k - 1, q);
 		combination(n - 1, k, q);
 	}
 }
 
+void recursive_combination(int n, int k, int q) {
+    if (k == 0) {
+        count2++;
+        for (int i = 0; i < q; i++) 
+            printf("%d ", t2[i]);
+        printf("\n");
+    }
+    else if (n == 0) return;
+    else {
+        t2[k - 1] = data[n - 1];
+        recursive_combination(n, k - 1, q);
+        recursive_combination(n - 1, k, q);
+    }
+}
+
 
 int main() {
-	n = 10;
-	count = 0;
+	n = 5;
+	count1 = 0;
 	for (int i = 0; i < n; i++) {
 		data[i] = i + 1;
 	}
-	combination(n, 2, 2);
-	printf("%d\n", count);
+    // nC2
+	combination(n, 3, 3);
+	printf("%d\n", count1);
+    
+    // nHr
+    recursive_combination(n, 2, 2);
+    printf("%d\n", count2);
 }
---
+++ Basic/Combination/Readme.md
@@ -0,0 +1,19 @@
+# Combination
+
+
+
+* The number of cases where k can be selected from n
+* The folmula as below
+
+
+$$
+{n\choose k}  == {\frac {P(n,k)}{k!}}={\frac {n!}{k!\cdot (n-k)!}}
+$$
+
+
+* Implemented as full search for all possible combinations, not simple number of cases
+
+
+
+* Recursive combination allows duplication of elements.
+
