5b05c8ad0f79a41f305a1c3f643e04f7b771d093
jeothen <jeothen@gmail.com>
2020-12-12 23:28:34 +0900

    Prim algorithm

--- Basic/MST/Prim/Prim.cpp
+++ Basic/MST/Prim/Prim.cpp
@@ -1,50 +1,68 @@
-#include <stdio.h>
-#pragma warning(disable:4996)
+#include <iostream>
+#include "string.h"
+#include <queue>
+using namespace std;
 
-// pq를 쓰고 edge node로 시간 복잡도를 kruskal만큼 줄일 수 있다
+#define INF 1e9
+typedef pair<int,pair<int,int>> pp;
+const int nodecount = 1001;
+int dist[nodecount], start, idx;
+vector<pp> vc[nodecount];
 
-int graph[1001][1001];
-int dist[1001];
+int prim(){
+	priority_queue<pp, vector<pp>, greater<pp>> pq;
+	for (auto& p : vc[start]) pq.push(p);  // start node edge upload
+	fill_n(dist,sizeof(dist),INF);
+	int ret = 0;
+	dist[start] = -1;
 
-int main() {
-	freopen("input.txt", "r", stdin);
-	int n, m;
-	scanf("%d", &n);
-	scanf("%d", &m);
-	int a, b, c;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			graph[i][j] = 1000000;
-			if (i == j) graph[i][j] = 0;
+	while(!pq.empty()){
+		int val = pq.top().first;
+		pair<int,int> node = pq.top().second;
+		if (dist[node.first] == -1) continue; // already visited
+		dist[node.first] = -1; // start visited check
+		pq.pop();
+		for (auto& p: vc[node.second]) {
+			if (dist[p.second.first] != -1) // not visited
+				pq.push(p);
 		}
+		ret += val;
+		// if n count break;
 	}
-	for (int i = 0; i < m; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-		graph[a][b] = graph[b][a] = c;
-	}
+	return ret;
+}
 
-	int start = 1;
-	int dist[1001];
-	int value = 0;
-	for (int i = 1; i <= n; i++) dist[i] = graph[1][i];
-	dist[1] = -1;
-	int minindex = 1;
-	for (int i = 1; i < n; i++) {
-		int minimum = 10002;
-		for (int j = 1; j <= n; j++) {
+
+
+int square_n(){
+	int graph[nodecount][nodecount];
+	fill_n(graph, sizeof(graph),INF); // default : not arrived
+	// input graph a->b = c
+	for (int i=1; i<=nodecount; i++){
+        graph[i][i] = 0;
+    }    
+	int ret = 0;
+	for (int i = 1; i <= nodecount; i++)
+		dist[i] = graph[start][i]; // if not arrived, INF
+
+	dist[start] = -1; // -1 is visited
+	for (int i = 1; i < nodecount; i++) // mst need to find minimum distance from node to node 
+	{
+		int minimum = INF;
+		for (int j = 1; j <= nodecount; j++)
+		{
 			if (dist[j] != -1) {
 				if (minimum > dist[j]) {
 					minimum = dist[j];
-					minindex = j;
+					idx = j;
 				}
 			}
 		}
-		value += minimum;
-		dist[minindex] = -1;
-		for (int j = 1; j <= n; j++) {
-			if (dist[j] > graph[minindex][j]) dist[j] = graph[minindex][j];
+		ret += minimum;  // plus minimum distance
+		dist[idx] = -1; // idx is visited (minimum distance index)
+		for (int j = 1; j <= nodecount; j++) { // update if  idx to j is smaller than original distance to j
+			if (dist[j] > graph[idx][j]) dist[j] = graph[idx][j];
 		}
 	}
-	printf("%d\n", value);
-	return 0;
+	return ret;
 }
---
+++ Basic/MST/Prim/Readme.md
@@ -0,0 +1,5 @@
+# Prim
+
+* It is a representative algorithm of MST and operates based on node.
+* The worst time complexity is O(n^2), which is the square of the number of nodes.  / n is nodecount
+* e, edge count, is always smaller than n^2, so worst time complexity is O(elogn) when using priority queue.
