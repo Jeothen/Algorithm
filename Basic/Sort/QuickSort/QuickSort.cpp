36f149ff2e42efe545c295b70f4caaa56a56c736
jeothen <jeothen@gmail.com>
2020-12-19 02:26:06 +0900

    quicksort source code

--- Basic/Sort/QuickSort/QuickSort.cpp
+++ Basic/Sort/QuickSort/QuickSort.cpp
@@ -2,28 +2,28 @@
 #include <algorithm>
 using namespace std;
 
-
-void swap(int* a, int* b){
-	int* tmp = a;
-	a = b; b = tmp;
-}
-
 void quicksort(int *arr, int start, int end) {
 	int left = start;
 	int right = end;
 	int pivot = arr[(left + right) / 2];
-	for (int i = 0; i < 9; i++) printf("%d ", arr[i]);
-	printf("\n");
-	printf("left = %d , pivot = %d, right = %d\n", left, pivot, right);
-	while (left < right) {
+	while (left < right)
+	{
 		while (arr[left] < pivot) left++;
 		while (arr[right] > pivot) right--;
 		if (left <= right) {
-			swap(&arr[left], &arr[right]);
+			swap(arr[left], arr[right]);
 			++left;
 			--right;
 		}
 	}
 	if (start < right) quicksort(arr, start, right);
 	if (left < end) quicksort(arr, left, end);
+}
+
+int main(){
+	int arr[] = {2, 1, 7, 13, 5, 1, 242, 63, 11, 2};
+	quicksort(arr,0,10-1);
+	for (int i=0; i<10; i++){
+		cout << arr[i] << " ";
+	}	
 }
