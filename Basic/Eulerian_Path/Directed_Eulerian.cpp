af795aa62e32c166ab8a8303a800c5ea1ca50667
jeothen <jeothen@gmail.com>
2021-01-26 00:58:28 +0900

    directed eulerian;

--- Basic/Eulerian_Path/Directed_Eulerian.cpp
+++ Basic/Eulerian_Path/Directed_Eulerian.cpp
@@ -5,51 +5,65 @@ using namespace std;
 const int max_Ver = 4;
 
 bool visited[max_Ver];
-vector<int>adj[max_Ver];
+vector<int> adj[max_Ver];
 
-void dfs(vector<int>* vc, int idx){
 
-}
-
-vector<int>* create_rev_adj(){  
-    vector<int>rev[max_Ver];
+void create_rev_adj(){  
+    vector<int> rev[max_Ver];
     for (int i=0; i<max_Ver;i++){
         for (int j=0; j<adj[i].size();j++){
             rev[j].push_back(i);
         }
+        adj[i].clear();
     }
-    return rev;
+    for (int i=0; i<max_Ver;i++){
+        for (int j=0; j<rev[i].size();j++){
+            adj[i].push_back(rev[i][j]);
+        }
+    }
+}
+
+void dfs(int idx, bool visited[])
+{
+    visited[idx] = true;
+    for (auto s = adj[idx].begin(); s != adj[idx].end(); s++){
+        if (!visited[*s]) dfs(*s,visited);
+    }
 }
 
 bool is_connected(){
-
     memset(visited, 0, sizeof(visited));
     int idx;
     for (idx = 0; idx < max_Ver; idx++)
     {
-        if (adj[idx].size() != 0)
+        if (adj[idx].size() != 0)  // first vertex is idx
             break;
     }
-    dfs(adj,idx);
+    dfs(idx,visited);
     for (int i = 0; i < max_Ver; i++)
     {
         if (!visited[i] && adj[i].size())
             return false; // if node exists but not visited
     }
+
     // reverse graph
-    vector<int> *rev_adj;
-    rev_adj = create_rev_adj(adj);
-
-
+    memset(visited, 0, sizeof(visited));
+    create_rev_adj();
+    dfs(idx,visited);
+    for (int i = 0; i < max_Ver; i++)
+    {
+        if (!visited[i] && adj[i].size())
+            return false; // if node exists but not visited
+    }
+    return true;
 } 
 
 int check_eulerian()
 {
     if (!is_connected()) return 0;
-
+    return true;
     vector<int> inward(max_Ver, 0);
     vector<int> outward(max_Ver, 0);
-
     for (int i = 0; i < max_Ver; i++)
     {
         for (int j = 0; j < max_Ver; j++)
@@ -86,7 +100,6 @@ int check_eulerian()
 
 void init()
 {
-    memset(visited, 0, sizeof(visited));
     if (check_eulerian() == 2) printf("Eulerian Cycle");
     else if (check_eulerian() == 1) printf("Eulerian Path");
     else printf("Not Eulerian");
