ef0b48c5c6e19c147ae71b24af7ff00b0f0477d2
jeothen <jeothen@gmail.com>
2021-01-25 23:59:24 +0900

    euler

--- Basic/Eulerian_Path/Directed_Eulerian.cpp
+++ Basic/Eulerian_Path/Directed_Eulerian.cpp
@@ -4,15 +4,43 @@
 using namespace std;
 const int max_Ver = 4;
 
-vector<int> adj[max_Ver];
 bool visited[max_Ver];
+vector<int>adj[max_Ver];
 
-void dfs(){
+void dfs(vector<int>* vc, int idx){
 
 }
 
+vector<int>* create_rev_adj(){  
+    vector<int>rev[max_Ver];
+    for (int i=0; i<max_Ver;i++){
+        for (int j=0; j<adj[i].size();j++){
+            rev[j].push_back(i);
+        }
+    }
+    return rev;
+}
+
 bool is_connected(){
-    
+
+    memset(visited, 0, sizeof(visited));
+    int idx;
+    for (idx = 0; idx < max_Ver; idx++)
+    {
+        if (adj[idx].size() != 0)
+            break;
+    }
+    dfs(adj,idx);
+    for (int i = 0; i < max_Ver; i++)
+    {
+        if (!visited[i] && adj[i].size())
+            return false; // if node exists but not visited
+    }
+    // reverse graph
+    vector<int> *rev_adj;
+    rev_adj = create_rev_adj(adj);
+
+
 } 
 
 int check_eulerian()
@@ -24,18 +52,35 @@ int check_eulerian()
 
     for (int i = 0; i < max_Ver; i++)
     {
-        int sum = 0;
         for (int j = 0; j < max_Ver; j++)
         {
             if (adj[i][j])
             {
                 inward[j]++; 
-                sum++;       
+                outward[i]++;
             }
         }
-        outward[i] = sum;
     }
+    bool path_in = false;
+    bool path_out = false;
 
+    bool chk_path = true;
+    bool chk_cycle = true;
+    for (int i=0;i<max_Ver;i++){
+        if (inward[i] != outward[i]) {
+            chk_cycle = false;
+            if (inward[i]+1 == outward[i]) {
+                if (path_in) { chk_path = false; break; }
+                path_in = true;
+            }
+            else if (inward[i] == outward[i] + 1){
+                if (path_out) { chk_path = false; break; }
+                path_out = true;
+            }
+        }
+    }
+    if (chk_cycle) return 2;
+    if (chk_path && path_in && path_out) return 1;
     return 0;
 }
 
