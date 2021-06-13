60380f95ace21bf19be2e14d19bf6fe29ce542d8
jeothen <jeothen@gmail.com>
2021-06-14 01:07:17 +0900

    BOJ 5651

--- BOJ/Maximum_Flow/5651.cpp
+++ BOJ/Maximum_Flow/5651.cpp
@@ -6,14 +6,15 @@
 #define START 1
 #define INF 1'000'000'000
 
-int F[MAX_NODE][MAX_NODE], C[MAX_NODE][MAX_NODE], CNT[MAX_NODE][MAX_NODE];
-std::vector<int>adj[MAX_NODE];
-int path[MAX_NODE];
+int F[MAX_NODE][MAX_NODE], C[MAX_NODE][MAX_NODE];
+std::vector<int> adj[MAX_NODE];
+std::vector<std::pair<int,int>> edges;
 
 void init()
 {
     memset(F,0,sizeof(F));
     memset(C,0,sizeof(C));
+    edges.clear();
     for (int i=0;i<MAX_NODE;i++) adj[i].clear();
 }
 
@@ -25,19 +26,22 @@ int min(int a, int b)
 bool other_path(int source, int target)
 {
     std::queue<int>q; q.push(source);
+    std::vector<int> prev(MAX_NODE, -1);
     while(!q.empty())
     {
         int cur = q.front(); q.pop();
         for (auto next : adj[cur])
         {
-            if (!(cur == source && next == target) && C[cur][next] > F[cur][next]) 
+            if (prev[next] == -1 && C[cur][next] > F[cur][next]) 
             {
-                if (next == target) return true;
                 q.push(next);
+                prev[next] = cur;
+                if (next == target) return true;
             }
         }
     }
-    return false;
+    if (prev[target] == -1) return false;
+    return true;
 }
 
 void maximum_flow(int END)
@@ -60,7 +64,6 @@ void maximum_flow(int END)
             }
         }
         if (prev[END] == -1) break; // Not visit target
-        for (int i = END; i != START; i = prev[i]) path[i] = prev[i];
         int min_flow = INF;
         for (auto i = END; i != START; i = prev[i])
         {
@@ -69,6 +72,7 @@ void maximum_flow(int END)
         for (int i = END; i != START; i = prev[i])
         {
             F[prev[i]][i] += min_flow;
+            F[i][prev[i]] -= min_flow;
         }
     }
 }
@@ -86,14 +90,16 @@ int main()
         {
             scanf("%d %d %d",&f,&t,&b);
             adj[f].push_back(t);
-            C[f][t] = b;
-            CNT[f][t] += 1;
+
+            adj[t].push_back(f); // reverse way
+            C[f][t] += b;
+            edges.push_back({f,t});
         }
         maximum_flow(N);
         int res = 0;
-        for (int i= N; i != START; i = path[i]) 
+        for (int i= 0; i < edges.size() ; i++) 
         {
-            if ( C[path[i]][i] == F[path[i]][i] && !other_path(path[i], i)) res += CNT[path[i]][i];
+            if (!other_path(edges[i].first, edges[i].second)) res++;
         }
         printf("%d\n",res);
     }
